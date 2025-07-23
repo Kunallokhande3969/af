35c92cfeedb497fe995b4cb868a86f767c6b992f
kunal lokhande <kunallokhande3969@gmail.com>
2025-07-23 12:14:43 -0900

    first commit

--- utiles/axios.js
+++ utiles/axios.js
@@ -2,18 +2,16 @@ import axios from "axios";
 
 // Create an Axios instance
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/", // Use environment variable
-  withCredentials: true, // Include credentials (cookies) in requests
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/",
+  withCredentials: true,
   headers: {
-    "Content-Type": "application/json", // Default content type
-    Accept: "application/json", // Accept JSON responses
+    "Content-Type": "application/json",
+    Accept: "application/json",
   },
 });
 
-// Add a request interceptor
 instance.interceptors.request.use(
   (config) => {
-    // You can add authorization tokens here if needed
     const token = localStorage.getItem("authToken");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
@@ -21,21 +19,16 @@ instance.interceptors.request.use(
     return config;
   },
   (error) => {
-    // Handle request errors
     return Promise.reject(error);
   }
 );
 
-// Add a response interceptor
 instance.interceptors.response.use(
   (response) => {
-    // Handle successful responses
     return response;
   },
   (error) => {
-    // Handle errors globally
     if (error.response && error.response.status === 401) {
-      // Handle unauthorized errors (e.g., redirect to login)
       console.error("Unauthorized! Redirecting to login...");
     }
     return Promise.reject(error);
